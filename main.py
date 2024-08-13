d3bd621fe06a94078585616d32fa54ded38f9df8
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2024-08-13 18:32:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-import gdown
-import os
 
 st.set_page_config(
     page_title = "Tensorflow Model",
@@ -12,20 +10,12 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
 )
-
-def download_model():
-    url = f'https://drive.google.com/file/d/1bGKlH7y40zg-IbEr4mhAKYpeHV9a7_p9/view?usp=drive_link'
-    output = 'model.keras'
-    if not os.path.exists(output):
-        gdown.download(url, output, quiet=False)
-
+# Load the model once
 @st.cache_resource
 def load_model():
-    download_model()
     return tf.keras.models.load_model('model.keras')
 
-with st.spinner("Loading model"):
-    model = load_model()
+model = load_model()
 
 st.write('# MNIST Digit Recognition')
 st.write('## Using a CNN `TensorFlow` model')
@@ -36,7 +26,7 @@ stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 9)
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color='#FFFFFF',
     background_color='#000000',
@@ -48,20 +38,22 @@ canvas_result = st_canvas(
 )
 
 if canvas_result.image_data is not None:
+    # Convert the canvas image to grayscale directly
     input_numpy_array = np.array(canvas_result.image_data)
     input_image = Image.fromarray(input_numpy_array.astype('uint8'), 'RGBA')
     input_image_gs = input_image.convert('L')
 
+    # Convert to numpy array and normalize
     input_image_gs_np = np.asarray(input_image_gs, dtype=np.float32)
     image_pil = Image.fromarray(input_image_gs_np)
     new_image = image_pil.resize((28, 28))
-    input_image_gs_np = np.array(new_image)
+    input_image_gs_np = np.array(new_image) # Resize to 28x28 directly
 
-    tensor_image = np.expand_dims(input_image_gs_np, axis=-1)
-    tensor_image = np.expand_dims(tensor_image, axis=0)
+    tensor_image = np.expand_dims(input_image_gs_np, axis=-1)  # Add channel dimension
+    tensor_image = np.expand_dims(tensor_image, axis=0)  # Add batch dimension
 
-    mean, std = 0.1307, 0.3081
-    tensor_image = (tensor_image - mean) / std
+    mean, std = 0.1307, 0.3081  # Check if these are correct for your model
+    tensor_image = (tensor_image - mean) / std  # Normalize input
 
     # Perform prediction
     predictions = model.predict(tensor_image)
