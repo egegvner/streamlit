ff860207525ccad6df956564813f7670457913f2
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2024-08-15 10:53:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 time.sleep(0.1)
 tf.keras.backend.clear_session()
-model = tf.keras.models.load_model('model.keras')
+model = tf.keras.models.load_model('model.h5')
 
 st.write('# MNIST Digit Recognition')
 st.write('###### Using a CNN `TensorFlow` Model')
