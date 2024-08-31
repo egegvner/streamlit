69ef58b379577ec9ad927f0261aa4f3c6a2450dc
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2024-08-31 17:10:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from streamlit_drawable_canvas import st_canvas
 import streamlit as st
 
-st.markdown('''
+st.markdown("""
 <!-- Start of CuterCounter Code -->
 <a href="https://www.cutercounter.com/" target="_blank"><img src="https://www.cutercounter.com/hits.php?id=hxpcokn&nd=9&style=1" border="0" alt="website counter"></a>
 <!-- End of CuterCounter Code -->
-''')
+""", unsafe_allow_html=True)
