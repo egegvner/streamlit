61c1ff4d6570bf52f0d52ba6e7d8be1930b8b7ec
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2024-09-02 21:18:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ if canvas_result.image_data is not None:
     
     col1.metric(label="Epochs", value=10, delta=9, help="One epoch refers to one complete pass through the entire training dataset.")
 
-    col2.metric(label="Accuracy", value="99.76%", delta="0.52%", help="Total accuracy of the model which is calculated based on the test data.")
+    col2.metric(label="Accuracy", value="96.76%", delta="0.52%", help="Total accuracy of the model which is calculated based on the test data.")
 
     col3.metric(label="Model Train Time", value="0.18h", delta="0.4h", help="Time required to fully train the model with specified epoch value. (in hours)", delta_color="inverse")
 
