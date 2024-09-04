63aee118b79f55ee6b7699d2f34d200934a90a88
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2024-09-04 16:44:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ data = {
 st.write('# MNIST Digit Recognition')
 st.write('###### Using a `CNN Keras` Model')
 
-st.write('#### ** The current model is outdated, will be re-trained soon.')
+st.write('#### ** The current model is outdated, will be re-trained soon')
 
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",
